bd7be415b30298c393671c1141e00ffc735aedd7
Ashish Birajdar <birajdarashish2020@gmail.com>
2026-04-20 01:24:06 +0530

    huge commit

--- install.ps1
+++ install.ps1
@@ -10,14 +10,18 @@ $repoBase = "https://raw.githubusercontent.com/ashleyxdev/stqa-suite/master"
 # ID for practicals = their filename prefix number
 # ID for setup files = 99, 100, ... (to avoid conflict with future practicals)
 $labFiles = @(
-    @{ id = 5;  name = "5-LoginTest.java";            type = "Java";     section = "Practical" },
-    @{ id = 7;  name = "7-update-student-records.py"; type = "Python";   section = "Practical" },
-    @{ id = 8;  name = "8-select-student-marks.py";   type = "Python";   section = "Practical" },
-    @{ id = 9;  name = "9-CountObjects.java";         type = "Java";     section = "Practical" },
-    @{ id = 10;  name = "10-CountListItems.java";     type = "Java";     section = "Practical" },
-    @{ id = 11;  name = "11-CountCheckboxes.java";    type = "Java";     section = "Practical" },
-    @{ id = 99; name = "selenium-setup-guide.md";     type = "Markdown"; section = "Setup"     },
-    @{ id = 100; name = "pom.xml";                    type = "XML";      section = "Setup"     }
+    @{ id = 1;  name = "1-gmail-login-test-suite";          type = "Excel";    section = "Practical" },
+    @{ id = 2;  name = "2-two-website-test-suite";          type = "Excel";    section = "Practical" },
+    @{ id = 3;  name = "3-social-media-app-test-suite";     type = "Excel";    section = "Practical" },
+    @{ id = 4;  name = "4-defect-report";                   type = "Excel";    section = "Practical" },
+    @{ id = 5;  name = "5-LoginTest.java";                  type = "Java";     section = "Practical" },
+    @{ id = 7;  name = "7-update-student-records.py";       type = "Python";   section = "Practical" },
+    @{ id = 8;  name = "8-select-student-marks.py";         type = "Python";   section = "Practical" },
+    @{ id = 9;  name = "9-CountObjects.java";               type = "Java";     section = "Practical" },
+    @{ id = 10;  name = "10-CountListItems.java";           type = "Java";     section = "Practical" },
+    @{ id = 11;  name = "11-CountCheckboxes.java";          type = "Java";     section = "Practical" },
+    @{ id = 99; name = "selenium-setup-guide.md";           type = "Markdown"; section = "Setup"     },
+    @{ id = 100; name = "pom.xml";                          type = "XML";      section = "Setup"     }
 )
 
 # Display: Lab Practicals
